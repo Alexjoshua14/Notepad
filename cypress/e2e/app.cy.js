db867b19d877889bdeeb4ca1cd0f8f2624bd5ffc
Alex <ajoshua14@icloud.com>
2023-07-25 11:59:32 -0700

    Add cypress for e2e testing

---
+++ cypress/e2e/app.cy.js
@@ -0,0 +1,21 @@
+
+
+describe('Navigation', () => {
+  it('should navigate home', () => {
+    cy.visit('http://localhost:3000/profile')
+    cy.get('h1').should('contain', 'Notepad')
+
+    cy.get('#homeLink').click()
+    cy.url().should('eq', 'http://localhost:3000/')
+  })
+})
+
+describe('Authentication', () => {
+  it('should navigate user to Github OAuth', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('#loginButton').click()
+    cy.url().should('include', 'github.com')
+
+    
+  })
+})
