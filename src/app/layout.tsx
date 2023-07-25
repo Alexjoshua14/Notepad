99ed8d8f0a6038ff3189c489c7c4eecb6ac1fe9a
Alex <ajoshua14@icloud.com>
2023-07-24 18:24:39 -0700

    Center main content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="overscroll-none">
-      <body className={`${inter.className} flex min-h-screen max-w-screen overflow-x-hidden bg-gradient-to-tr from-teal-400 to-purple-700 text-white`}>
+      <body className={`${inter.className} flex center min-h-screen max-w-screen overflow-x-hidden bg-gradient-to-tr from-teal-400 to-purple-700 text-white`}>
         <Nav />
         <div className="flex-1 flex center max-w-full mx-4 md:mx-48 xl:mx-96 pt-24 pb-12">
           {children}
