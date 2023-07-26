9a1928f768a6095dc039f01c21a2a6ad79ae1e1a
Alex <ajoshua14@icloud.com>
2023-07-26 12:13:55 -0700

    Fix parameter error

--- src/app/new_note/page.tsx
+++ src/app/new_note/page.tsx
@@ -2,17 +2,14 @@
 import { NewPost } from '@/components/NewPost';
 import { Post } from '@/types';
 
-interface NewNoteProps {
-  post?: Post;
-}
 
-export default function NewNote({ post }: NewNoteProps) {
+export default function NewNote() {
   return (
     <main className="flex-1 h-full w-full flex center">
       <section className="section-main">
         <h1 className="header">{`What's on your mind?`}</h1>
         <div className="flex-1 flex center">
-          <NewPost post={post} />
+          <NewPost />
         </div>
       </section>
     </main>
